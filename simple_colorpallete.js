4d8fbfba234c966e59ae49feb656168ceb49d8c6
Thinya <103097600+Thinya@users.noreply.github.com>
2022-05-07 19:09:16 +0900

    Update simple_colorpallete.js

--- simple_colorpallete.js
+++ simple_colorpallete.js
@@ -5,3 +5,6 @@ let pallete = ['#744199','#009250','#FFE600','#DA5019','#DCCAE8'];
 
 //🎴花札の色。10色、似ている色あり
 let hanahuda = ['#93aeca','#e68d36','#8f4d66','#41312c','#e7b7a6','#e7e04c','#9aac49','#814554','#dc4d30','#fbef3f',];
+
+//淡い色,白を含めた7色
+let awa = ['#FFFFFF','#F6D4D8','#DEDFEF','#EEF5D3','#C9E8F1','#C6EDDB','#CBE6F3'];
